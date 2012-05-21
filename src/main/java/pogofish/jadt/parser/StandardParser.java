1f60a904daea358e665b894da8e8d115a6a5d07f
James Iry <jamesiry@gmail.com>
2012-05-21 07:02:12 -0700

    License with Apache 2.0

--- src/main/java/pogofish/jadt/parser/StandardParser.java
+++ src/main/java/pogofish/jadt/parser/StandardParser.java
@@ -1,3 +1,18 @@
+/*
+Copyright 2012 James Iry
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package pogofish.jadt.parser;
 
 import java.io.*;
@@ -38,7 +53,7 @@ public class StandardParser implements Parser {
             tokenizer = new StreamTokenizer(reader);
             tokenizer.resetSyntax();
             tokenizer.slashSlashComments(true);
-            tokenizer.slashSlashComments(true);
+            tokenizer.slashStarComments(true);
             tokenizer.wordChars('a', 'z');
             tokenizer.wordChars('A', 'Z');
             tokenizer.wordChars('0', '9');
--- src/test/java/pogofish/jadt/emitter/StringTargetFactory.java
+++ src/test/java/pogofish/jadt/emitter/StringTargetFactory.java
@@ -1,9 +1,21 @@
+/*
+Copyright 2012 James Iry
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package pogofish.jadt.emitter;
 
 import java.util.*;
-
-import pogofish.jadt.emitter.Target;
-import pogofish.jadt.emitter.TargetFactory;
 
 public class StringTargetFactory implements TargetFactory {
     private Map<String, String> results = new HashMap<String, String>();

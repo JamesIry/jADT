7a7b879c1054556294f83f987d81dd448405ddcd
James Iry <jamesiry@gmail.com>
2012-06-10 20:09:26 -0700

    organized documentation into a manual

---
+++ JADT-samples/src/main/java/pogofish/jadt/samples/visitor/MyBooleanExamples.java
@@ -0,0 +1,23 @@
+package pogofish.jadt.samples.visitor;
+
+public class MyBooleanExamples {
+    public MyBoolean computeABoolean() {
+        return new MyTrue();
+    }
+    
+    // START SNIPPET: getAString
+    public String getAString(MyBoolean x) {
+        return x.accept(new MyBooleanVisitor<String>() {
+            @Override
+            public String visitMyTrue(MyTrue x) {
+                return "yup";
+            }
+
+            @Override
+            public String visitMyFalse(MyFalse x) {
+                return "nope";
+            }
+        });
+    }
+    // END SNIPPET: getAString
+}
